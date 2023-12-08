eec0b35ec8287b25f55705e67fb2b88c75a9f635
user <ahmadboyabdurahimov589@gmail.com>
2023-12-08 19:26:33 +0500

    new changes

--- common/admin.py
+++ common/admin.py
@@ -31,4 +31,7 @@ class WorkflowAdmin(admin.ModelAdmin):
 
 @admin.register(models.Status)
 class salom(admin.ModelAdmin):
-    pass
+
+    def has_add_permission(self, request): return False
+    def has_delete_permission(self, request, obj=None): return False
+    def has_change_permission(self, request, obj=None): return False
